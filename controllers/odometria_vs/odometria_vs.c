3f82a8565c1c5c0a47801e72231b43853385e562
Brais Barboza <brais.barboza@udc.es>
2022-04-15 15:41:22 +0200

    matrix_works
    matrix works going forward, not tested after a turn

--- controllers/odometria_vs/odometria_vs.c
+++ controllers/odometria_vs/odometria_vs.c
@@ -27,7 +27,7 @@
 #define CRUISE_SPEED 0.628
 #define SENSOR_SAMPLING_PERIOD 1
 #define DELTA 2.58
-#define MATRIX_LIMIT 100
+#define MATRIX_LIMIT 40
 #define WALL_THRESHOLD 230.000
    /*
    * This is the main program.
@@ -53,11 +53,11 @@ void init_map(int map[][MATRIX_LIMIT]) {
 		}
 	}
 }
-void advance1(WbDeviceTag left_motor, WbDeviceTag right_motor, double* before_movement_sensor_state) {
-	wb_motor_set_position(left_motor, before_movement_sensor_state[0] + CELL_SIZE / WHEEL_RADIX);
-	wb_motor_set_position(right_motor, before_movement_sensor_state[1] + CELL_SIZE / WHEEL_RADIX);
+void advance1(WbDeviceTag left_motor, WbDeviceTag left_sensor, WbDeviceTag right_motor, WbDeviceTag right_sensor) {
+	wb_motor_set_position(left_motor, wb_position_sensor_get_value(left_sensor) + CELL_SIZE / WHEEL_RADIX);
+	wb_motor_set_position(right_motor, wb_position_sensor_get_value(right_sensor) + CELL_SIZE / WHEEL_RADIX);
 }
-void turn(WbDeviceTag left_motor, WbDeviceTag right_motor, int direction, double* before_movement_sensor_state) {
+void turn(WbDeviceTag left_motor, WbDeviceTag right_motor, int direction, double before_movement_sensor_state[]) {
 	if (!direction)
 	{
 		wb_motor_set_position(left_motor, -PI / 2 * DELTA + before_movement_sensor_state[0]);
@@ -72,22 +72,26 @@ void turn(WbDeviceTag left_motor, WbDeviceTag right_motor, int direction, double
 	//printf("%f\n", -PI / 2 * DELTA + before_movement_sensor_state[0]);
 }
 
-
-int stopped(WbDeviceTag left_sensor, WbDeviceTag right_sensor, double* sensors_previous_position) {
-	double left_sensor_current_position = wb_position_sensor_get_value(left_sensor);
-	double right_sensor_current_position = wb_position_sensor_get_value(right_sensor);
-	//printf("%f, %f, %f, %f \n", left_sensor_current_position, sensors_previous_position[0], right_sensor_current_position, sensors_previous_position[1]);
-	if (left_sensor_current_position == sensors_previous_position[0] && right_sensor_current_position == sensors_previous_position[1]);
-	{
-		return 1;
-	}
-
-	return 0;
-
+int stopped(WbDeviceTag left_motor, WbDeviceTag left_sensor, WbDeviceTag right_motor, WbDeviceTag right_sensor) {
+	double left_motor_target_position = wb_motor_get_target_position(left_motor);
+	double right_motor_target_position = wb_motor_get_target_position(right_motor);
+	return ((int)(left_motor_target_position * 100 + .5) == (int)(wb_position_sensor_get_value(left_sensor) * 100 + .5)) && ((int)(right_motor_target_position * 100 + .5) == (int)(wb_position_sensor_get_value(right_sensor) * 100 + .5));
 }
-
-void update_map(WbDeviceTag infrared_sensors[], int orientation, int map[][MATRIX_LIMIT], int location[]) {
-	map[location[0]][location[1]] = 1;
+void print_matrix(int map[][MATRIX_LIMIT]) {
+	int row, columns;
+	for (row = 0; row < MATRIX_LIMIT; row++)
+	{
+		for (columns = 0; columns <MATRIX_LIMIT; columns++)
+		{
+			printf("%d	", map[row][columns]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	printf("\n");
+}
+void update_map(WbDeviceTag infrared_sensors[], int orientation, int map[][MATRIX_LIMIT], int location[], int blocked[]) {
+	map[location[0]][location[1]] = 2;
 	int map_value;
 	switch (orientation)
 	{
@@ -95,8 +99,7 @@ void update_map(WbDeviceTag infrared_sensors[], int orientation, int map[][MATRI
 		for (int i = 0; i < 4; i++)
 		{
 			map_value = wb_distance_sensor_get_value(infrared_sensors[i]) > WALL_THRESHOLD;
-			if (map_value)
-			{
+			blocked[i] = map_value;
 				switch (i)
 				{
 				case 0:
@@ -117,15 +120,14 @@ void update_map(WbDeviceTag infrared_sensors[], int orientation, int map[][MATRI
 				default:
 					break;
 				}
-			}
+			
 		}
 		break;
 	case 1:
 		for (int i = 0; i < 4; i++)
 		{
 			map_value = wb_distance_sensor_get_value(infrared_sensors[i]) > WALL_THRESHOLD;
-			if (map_value)
-			{
+			blocked[i] = !map_value;
 				switch (i)
 				{
 				case 0:
@@ -146,16 +148,14 @@ void update_map(WbDeviceTag infrared_sensors[], int orientation, int map[][MATRI
 				default:
 					break;
 				}
-			}
+			
 		}
 		break;
 	case 2:
 		for (int i = 0; i < 4; i++)
 		{
 			map_value = wb_distance_sensor_get_value(infrared_sensors[i]) > WALL_THRESHOLD;
-
-			if (map_value)
-			{
+			blocked[i] = !map_value;
 				switch (i)
 				{
 				case 0:
@@ -175,7 +175,7 @@ void update_map(WbDeviceTag infrared_sensors[], int orientation, int map[][MATRI
 					break;
 				default:
 					break;
-				}
+				
 			}
 		}
 		break;
@@ -183,9 +183,7 @@ void update_map(WbDeviceTag infrared_sensors[], int orientation, int map[][MATRI
 		for (int i = 0; i < 4; i++)
 		{
 			map_value = wb_distance_sensor_get_value(infrared_sensors[i]) > WALL_THRESHOLD;
-
-			if (map_value)
-			{
+			blocked[i] = !map_value;
 				switch (i)
 				{
 				case 0:
@@ -205,7 +203,7 @@ void update_map(WbDeviceTag infrared_sensors[], int orientation, int map[][MATRI
 				default:
 					break;
 				}
-			}
+			
 		}
 		break;
 	default:
@@ -213,13 +211,33 @@ void update_map(WbDeviceTag infrared_sensors[], int orientation, int map[][MATRI
 	}
 }
 
-void update_sensor_state(WbDeviceTag left_motor, WbDeviceTag right_motor, double* before_movement_sensor_state) {
-	if ((wb_motor_get_target_position(left_motor) == before_movement_sensor_state[0] + CELL_SIZE / WHEEL_RADIX) && (wb_motor_get_target_position(right_motor) == before_movement_sensor_state[1] + CELL_SIZE / WHEEL_RADIX)) {
-		before_movement_sensor_state[0] = before_movement_sensor_state[0] + CELL_SIZE / WHEEL_RADIX;
-		before_movement_sensor_state[1] = before_movement_sensor_state[1] + CELL_SIZE / WHEEL_RADIX;
-	}
-}
+void locate_forward(int orientation, int map[][MATRIX_LIMIT], int location[]) {
+	map[location[0]][location[1]] = 1;
+	switch (orientation)
+	{
+	case 0:
+		map[location[0] - 1][location[1]] = 2;
+		location[0] = location[0] - 1;
+		break;
+	case 1:
+		map[location[0]][location[1] - 1] = 2;
+		location[1] = location[1]-1;
 
+		break;
+	case 2:
+		map[location[0]][location[1] + 1] = 2;
+		location[1] = location[1]+1;
+
+		break;
+	case 3:
+		map[location[0] + 1][location[1]] = 2;
+		location[0] = location[0] + 1;
+		break;
+	default:
+		break;
+	}
+	printf("%d,%d\n", location[0], location[1]);
+}
 
 int main(int argc, char** argv) {
 	/* necessary to initialize webots stuff */
@@ -229,21 +247,18 @@ int main(int argc, char** argv) {
 	WbDeviceTag left_sensor = wb_robot_get_device("left wheel sensor");
 	WbDeviceTag right_sensor = wb_robot_get_device("right wheel sensor");
 	WbDeviceTag infrared_sensors[4] = { wb_robot_get_device("front infrared sensor") , wb_robot_get_device("left infrared sensor") ,wb_robot_get_device("right infrared sensor") ,wb_robot_get_device("rear infrared sensor") };
-	double* sensors_previous_position = (double*)malloc(sizeof(double) * 2);
-	double* before_movement_sensor_state = (double*)malloc(sizeof(double) * 2);
+
+	int blocked[4] = { 0,0,0,0 };
 	int map[MATRIX_LIMIT][MATRIX_LIMIT];
 	int* orientation;
-	int status = 0;
 	orientation = 0;
 	init_map(&map);
 	int location[2] = { MATRIX_LIMIT / 2,MATRIX_LIMIT / 2 };
 	int initial_location[2] = {location[0],location[1]};
 	map[location[0]][location[1]] = 1;
 	enable_sensors(left_sensor, right_sensor, &infrared_sensors);
-	int init[2] = { wb_position_sensor_get_value(left_sensor) ,wb_position_sensor_get_value(right_sensor) };
-	int init1[2] = { wb_position_sensor_get_value(left_sensor) ,wb_position_sensor_get_value(right_sensor) };
-	sensors_previous_position = &init;
-	before_movement_sensor_state = &init1;
+	double sensors_previous_position[2] = {0,0 };
+	double before_movement_sensor_state[2] = { 0,0 };
 
 	wb_motor_set_velocity(left_motor, 0.1 * MAX_SPEED);
 	wb_motor_set_velocity(right_motor, 0.1 * MAX_SPEED);
@@ -272,26 +287,24 @@ int main(int argc, char** argv) {
 		  * wb_motor_set_position(my_actuator, 10.0);
 		  */
 		  
-		if (stopped(left_sensor, right_sensor, &sensors_previous_position))
+		if (stopped(left_motor, left_sensor,right_motor,right_sensor))
 		{
-			update_map(infrared_sensors, orientation, &map, location);
-			update_sensor_state(left_motor, right_motor, &before_movement_sensor_state);
-			if (location!= initial_location)
-			{
-				if (!status)
-				{
-					if (wb_distance_sensor_get_value(infrared_sensors[0]) > WALL_THRESHOLD) {
-						advance1(left_motor, right_motor, &before_movement_sensor_state);
-					}
-				}
+			update_map(infrared_sensors, orientation, &map, location, blocked);
+			print_matrix(&map);
+			if (!blocked[0]) {
+				advance1(left_motor,left_sensor, right_motor,right_sensor);
+				locate_forward(orientation, &map, &location);
 			}
 			//turn(left_motor, right_motor, 0, &before_movement_sensor_state);
 		}
+		else
+		{
+			
+		}
 
+	sensors_previous_position[0] = wb_position_sensor_get_value(left_sensor);
+	sensors_previous_position[1] = wb_position_sensor_get_value(right_sensor);
 
-
-		sensors_previous_position[0] = wb_position_sensor_get_value(left_sensor);
-		sensors_previous_position[1] = wb_position_sensor_get_value(right_sensor);
 	};
 
 	/* Enter your cleanup code here */
